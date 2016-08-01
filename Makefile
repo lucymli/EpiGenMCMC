5343678432542134cf46e8cbf7cb24ea51864ef7
Lucy Li <mengqi.li09@imperial.ac.uk>
2016-08-01 14:06:25 +0100

    use a branching process model

--- Makefile
+++ Makefile
@@ -1,4 +1,44 @@
-CXX=clang-omp++
-RM=rm -f
-CPPPFLAGS=-fopenmp -lgsl
-main: main.o 
+CC=clang-omp++
+CFLAGS=-fopenmp -Imodels -C src
+LFLAGS=-liomp
+LDFLAGS=-L/usr/local/lib -L/Users/Lucy/libomp_oss/exports/mac_32e/lib.thin -lgsl
+
+BASEOBJ = data.o likelihood.o main.o MCMC.o model.o parameter.o particle.o pfilter.o trajectory.o
+SIROFFSPRINGOBJ = SIR_offspring_distribution.o
+
+SIRoffspring : $(BASEOBJ) $(SIROFFSPRINGOBJ)
+	$(CC) $(CFLAGS) $(LFLAGS) $(LDFLAGS) $(BASEOBJ) $(SIROFFSPRINGOBJ) -o SIRoffspring
+
+data.o : data.cpp data.h
+	$(CC) $(CFLAGS) data.cpp -o data.o
+
+likelihood.o : likelihood.h likelihood.cpp parameter.h
+	$(CC) $(CFLAGS) likelihood.cpp
+
+MCMC.o : MCMC.h MCMC.cpp pfilter.h model.h parameter.h trajectory.h data.h likelihood.h particle.h
+	$(CC) $(CFLAGS) MCMC.cpp
+
+model.o : model.h model.cpp trajectory.h parameter.h
+	$(CC) $(CFLAGS) model.cpp
+
+parameter.o : parameter.h parameter.cpp
+	$(CC) $(CFLAGS) parameter.cpp
+
+particle.o : particle.h particle.cpp trajectory.h
+	$(CC) $(CFLAGS) particle.cpp
+
+pfilter.o : pfilter.h pfilter.cpp model.h parameter.h trajectory.h data.h likelihood.h particle.h
+	$(CC) $(CFLAGS) trajectory.cpp
+
+trajectory.o : trajectory.h trajectory.cpp
+	$(CC) $(CFLAGS) trajectory.cpp
+
+SIR_offspring_distribution.o : model.h SIR_offspring_distribution.cpp
+	$(CC) $(CFLAGS) SIR_offspring_distribution.cpp
+
+main.o : main.cpp MCMC.h pfilter.h trajectory.h model.h parameter.h data.h likelihood.h particle.h
+	$(CC) $(CFLAGS) main.cpp
+
+clean : rm *.o *~ SIRoffspring
+
+tar : tar cfv *.h *.cpp models/SIR_offspring_distribution.cpp
--- src/models/branching_process_EbolaLiberia.cpp
+++ src/models/branching_process_EbolaLiberia.cpp
@@ -21,8 +21,10 @@ void Model::simulate(std::vector<double> & model_params, std::vector<std::string
     double R0_T0 = model_params[2];
     double R0_now = 0.0;
     double k = model_params[3];
-    double rateE2I = model_params[4];
-    double rateI2R = model_params[5];
+//    double rateE2I = model_params[4];
+//    double rateI2R = model_params[5];
+    double alpha = model_params[4];
+    double scale = model_params[5];
     double Re = 0.0;
     double recoveries=0.0;
     double new_infections=0.0;
@@ -32,42 +34,60 @@ void Model::simulate(std::vector<double> & model_params, std::vector<std::string
         int init_inf = (int)round(model_params[6]);
         traj->set_state(init_inf, 0);
         for (int i=0; i!=init_inf; ++i) {
-            durI = gsl_ran_exponential(rng, 1.0/rateE2I)+gsl_ran_exponential(rng, 1.0/rateI2R);
-            durI = start_dt + (int)round(durI/step_size);
+            durI = gsl_ran_gamma(rng, alpha, scale);
+            durI = (int)(durI/step_size);
             traj->add_recovery_time(durI);
         }
     }
-    // */
+    double num_infected = traj->get_state(0);
     for (int t=start_dt; t<end_dt; ++t) {
         //
         // Transitions
         //
         // Recoveries: I --> R
         recoveries = traj->num_recover_at(t-start_dt);
-        if (recoveries > 0) {
+        if (recoveries > 10000) { // If the epidemic is too large, set num_infected to 0, so that likelihood is 0.
+            num_infected = 0.0;
+        }
+        else if (recoveries > 0) {
             traj->set_traj(recoveries, t-start_dt);
             if (t*step_size<(R0_T0)) R0_now = R0_0;
             else R0_now = R0_1;
             new_infections = gsl_ran_negative_binomial(rng, k/(k+R0_now), k*recoveries);
-            if (new_infections > 0) {
-                for (int i=0; i!=new_infections; ++i) {
-                    durI = gsl_ran_exponential(rng, 1.0/rateE2I)+gsl_ran_exponential(rng, 1.0/rateI2R);
-                    durI = t + (int)round(durI/step_size);
-                    traj->add_recovery_time(durI-start_dt);
+            if (new_infections > 100) {
+                double infection_counter = 0;
+                double recover_after = 0.0;
+                for (int i=0; i!=total_dt; ++i) {
+                    double next_T = (double)(i+1)*step_size;
+                    double curr_T = (double) i*step_size;
+                    // deterministic calculation of how many people will recover
+                    recover_after = round(gsl_cdf_gamma_P(next_T, alpha, scale) - gsl_cdf_gamma_P(curr_T, alpha, scale) * new_infections);
+                    infection_counter += recover_after;
+                    if (infection_counter > new_infections) recover_after -= infection_counter-new_infections;
+                    traj->add_recovery_time(i+t-start_dt, (int)recover_after);
+                    if (infection_counter >= new_infections) break;
                 }
             }
-            traj->set_state(traj->get_state(0)+new_infections-recoveries, 0);
+            else if (new_infections > 0) {
+                for (int i=0; i!=new_infections; ++i) {
+                    durI = gsl_ran_gamma(rng, alpha, scale);
+                    durI = (int)(durI/step_size);
+                    traj->add_recovery_time(durI+t-start_dt);
+                }
+            }
+            num_infected += new_infections - recoveries;
         }
         double num_infected = traj->get_state(0);
         double curr_coal_rate = 1.0/num_infected;
         // Record 1/N for coalescent rate calculation
         if (num_infected > 0.0) {
-            traj->set_traj2(curr_coal_rate*Re/((1.0/rateI2R)+(1.0/rateE2I))*(1.0+1.0/k), t-start_dt);
+            traj->set_traj2(curr_coal_rate*Re/(alpha*scale)*(1.0+1.0/k), t-start_dt);
         }
         else {
             traj->set_traj2(0.0, t-start_dt);
             break;
         }
     }
+    traj->set_state(num_infected, 0);
     traj->delete_recoveries_before(end_dt-start_dt);
 }
