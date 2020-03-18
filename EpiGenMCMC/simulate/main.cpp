a866cb9d5d3cff5bcb23c91c337ed1ee13671151
Lucy Li <lucy.li@li-mbp.local>
2020-03-18 16:00:12 -0700

    switch back from poisson to binomial likelihood

---
+++ EpiGenMCMC/simulate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  simulate
+//
+//  Created by Lucy Li on 3/16/20.
+//  Copyright © 2020 Lucy M Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
